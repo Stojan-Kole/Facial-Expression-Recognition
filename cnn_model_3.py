a673ab8a9c621c35b97bc1122780c7fa649d33b8
Stojan <mi21131@alas.matf.bg.ac.rs>
2025-05-08 16:23:08 +0200

    added 3 take. Overfitting starting to improve

---
+++ cnn_model_3.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EmotionRecognition(nn.Module):
+    def __init__(self):
+        super(EmotionRecognition, self).__init__()
+
+        # Convolutional Block 1
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        # Convolutional Block 2
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.5)
+
+        # Dummy input to determine flatten size after conv layers
+        with torch.no_grad():
+            dummy = torch.zeros(1, 3, 48, 48)
+            dummy = self._forward_conv(dummy)
+            self.flattened_size = dummy.view(1, -1).size(1)
+
+        self.fc1 = nn.Linear(self.flattened_size, 512)
+        self.fc2 = nn.Linear(512, 7)
+
+    def _forward_conv(self, x):
+        # Only the convolutional part, used for shape inference and forward
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool(x)
+        return x
+
+    def forward(self, x):
+        x = self._forward_conv(x)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
